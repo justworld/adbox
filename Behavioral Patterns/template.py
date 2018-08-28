0a9b4be13aceaec2b59835cf0881218a71c0ab6d
justworld <jiangui1376044@126.com>
2018-08-28 09:48:45 +0800

    .

---
+++ Behavioral Patterns/template.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+"""
+模板方法模式
+"""
+import abc
+
+class People(object):
+    @abc.abstractmethod
+    def sex(self):
+        print 'no sex'
+    
+    def speak(self):
+        print 'i am human'
+    
+    __metaclass__ = abc.ABCMeta
+
+class Man(People):
+    def sex(self):
+        print 'man'
+
+class WoMan(People):
+    def sex(self):
+        print 'woman'
+
+if __name__ == '__main__':
+    Man().sex()
+    WoMan().sex()
